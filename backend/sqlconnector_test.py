0e6769c14219e93dd69d3af0c755fc6fdb8a5dc6
Andrew Chen <achen226@gmail.com>
2019-06-26 15:47:48 -0700

    fixed sqlconnector and added tests

---
+++ backend/sqlconnector_test.py
@@ -0,0 +1,11 @@
+from sqlconnector import DB_Connector
+
+# connect to DB
+con = DB_Connector("root", "ca$hm0ney", "roketto-dan.c0k9vwwy6vyu.us-west-1.rds.amazonaws.com", "roketto_dan")
+
+# query rows
+rows = con.getTable("datacenters")
+
+# print rows
+for dc in rows:
+    print(dc)
